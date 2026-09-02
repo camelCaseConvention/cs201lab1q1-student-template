0cd9f4bfabd4213818c706e6a7fc39533f74f84b
Hoszfit <lejie.tan.2025@smu.edu.sg>
2026-09-03 00:40:20 +0800

    MSG

--- NumbersArray.java
+++ NumbersArray.java
@@ -19,7 +19,38 @@ public class NumbersArray {
         System.out.println("Unique numbers : " + Arrays.toString(findUnique(input))); 
     }
 
-    // Write your methods here
+    int findMax(Integer[] nums){
+        int max = -1;
+        for(int n: nums) max = Math.max(n, max);
+        return max;
+    }
+    Integer[] findDuplicates(Integer[] input){
+        Arrays.sort(input);
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < input.length; i++){
+            int cnt = 1;
+            if(i < input.length - 1 && input[i] == input[i + 1]){
+                i++;
+                cnt++;
+            }
+            if(cnt > 1) ans.add(input[i]);
+        }
+        Integer[] arr = new Integer[ans.size()];
+        for(int i = 0; i < ans.size(); i++) arr[i] = ans.get(i);
+        return arr;
+    }
+    Integer[] findUnique(Integer[] input){
+        Arrays.sort(input);
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < input.length; i++){
+            if(i != 0 && input[i] == input[i - 1]) continue;
+            if(i != input.length - 1 && input[i] == input[i + 1]) continue;
+            ans.add(input[i]);
+        }
+        Integer[] arr = new Integer[ans.size()];
+        for(int i = 0; i < ans.size(); i++) arr[i] = ans.get(i);
+        return arr;
+    }
     
 }
 
