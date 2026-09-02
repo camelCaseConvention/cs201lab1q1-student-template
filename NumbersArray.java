fa4e23e0c273f44713a8afc4a12547a66c1a2681
Hoszfit <lejie.tan.2025@smu.edu.sg>
2026-09-03 00:55:00 +0800

    Test 2

--- NumbersArray.java
+++ NumbersArray.java
@@ -2,55 +2,64 @@ import java.util.*;
 
 public class NumbersArray {
     public static void main(String[] args) {
-        Integer[] input = {1,2,3,4,1,5};
+        Integer[] input = { 1, 2, 3, 4, 1, 5 };
 
         System.out.println("Input : " + Arrays.toString(input));
         System.out.println("Max number : " + findMax(input));
         System.out.println("Duplicate numbers : " + Arrays.toString(findDuplicates(input)));
-        System.out.println("Unique numbers : " + Arrays.toString(findUnique(input)));       
-        
+        System.out.println("Unique numbers : " + Arrays.toString(findUnique(input)));
+
         System.out.println();
 
-        input = new Integer[] {1,2,3,4,1,2,5,3};
+        input = new Integer[] { 1, 2, 3, 4, 1, 2, 5, 3 };
 
         System.out.println("Input : " + Arrays.toString(input));
         System.out.println("Max number : " + findMax(input));
         System.out.println("Duplicate numbers : " + Arrays.toString(findDuplicates(input)));
-        System.out.println("Unique numbers : " + Arrays.toString(findUnique(input))); 
+        System.out.println("Unique numbers : " + Arrays.toString(findUnique(input)));
     }
 
-    int findMax(Integer[] nums){
+    static int findMax(Integer[] nums) {
         int max = -1;
-        for(int n: nums) max = Math.max(n, max);
+        for (int n : nums)
+            max = Math.max(n, max);
         return max;
     }
-    Integer[] findDuplicates(Integer[] input){
+
+    static Integer[] findDuplicates(Integer[] iii) {
+        Integer[] input = Arrays.copyOf(iii, iii.length);
         Arrays.sort(input);
         ArrayList<Integer> ans = new ArrayList<>();
-        for(int i = 0; i < input.length; i++){
+        for (int i = 0; i < input.length; i++) {
             int cnt = 1;
-            if(i < input.length - 1 && input[i] == input[i + 1]){
+            while (i < input.length - 1 && input[i] == input[i + 1]) {
                 i++;
                 cnt++;
             }
-            if(cnt > 1) ans.add(input[i]);
+            if (cnt > 1)
+                ans.add(input[i]);
         }
         Integer[] arr = new Integer[ans.size()];
-        for(int i = 0; i < ans.size(); i++) arr[i] = ans.get(i);
+        for (int i = 0; i < ans.size(); i++)
+            arr[i] = ans.get(i);
         return arr;
     }
-    Integer[] findUnique(Integer[] input){
+
+    static Integer[] findUnique(Integer[] iii) {
+        Integer[] input = Arrays.copyOf(iii, iii.length);
         Arrays.sort(input);
         ArrayList<Integer> ans = new ArrayList<>();
-        for(int i = 0; i < input.length; i++){
-            if(i != 0 && input[i] == input[i - 1]) continue;
-            if(i != input.length - 1 && input[i] == input[i + 1]) continue;
+        for (int i = 0; i < input.length; i++) {
+            if (i != 0 && input[i] == input[i - 1])
+                continue;
+            if (i != input.length - 1 && input[i] == input[i + 1])
+                continue;
             ans.add(input[i]);
         }
         Integer[] arr = new Integer[ans.size()];
-        for(int i = 0; i < ans.size(); i++) arr[i] = ans.get(i);
+        for (int i = 0; i < ans.size(); i++)
+            arr[i] = ans.get(i);
         return arr;
     }
-    
-}
 
+}
